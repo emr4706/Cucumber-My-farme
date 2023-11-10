9a22cee21dcbc24748d920c80581b7aa7deeb878
emr4706 <emr4706@gmail.com>
2023-11-10 09:55:28 +0100

    change GWD settings for Jenkins

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -32,7 +33,15 @@ public class GWD {
                 case "firefox": threadDriver.set(new FirefoxDriver()); break; // ilgili threade bir driver set ettim
                 case "safari":  threadDriver.set(new SafariDriver());  break; // ilgili threade bir driver set ettim
                 case "edge":    threadDriver.set(new EdgeDriver());    break; // ilgili threade bir driver set ettim
-                default :  threadDriver.set(new ChromeDriver()); // ilgili threade bir driver set ettim
+                default :
+                    if (isRunningOnJenkins()) {
+                        FirefoxOptions options = new FirefoxOptions();
+                        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+                        threadDriver.set(new FirefoxDriver(options));
+                    }
+                    else {
+                        threadDriver.set(new ChromeDriver()); // ilgili threade bir driver set ettim
+                    }
             }
         }
 
@@ -58,6 +67,11 @@ public class GWD {
 
            threadDriver.set(driver); // kendisine null olarak ver, bu hatta bir dolu driver yok
         }
+    }
+
+    public static boolean isRunningOnJenkins() {
+        String jenkinsHome = System.getenv("JENKINS_HOME");
+        return jenkinsHome != null && !jenkinsHome.isEmpty();
     }
 
 

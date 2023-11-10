2237896721e2d52fc22136c7696b91e7adcd7df1
emr4706 <emr4706@gmail.com>
2023-11-10 10:11:56 +0100

    updated

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -37,7 +37,7 @@ public class GWD {
                 default :
                     ChromeOptions options = new ChromeOptions();
                         options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
-                        threadDriver.set(new ChromeDriver()); // ilgili threade bir driver set ettim
+                        threadDriver.set(new ChromeDriver(options)); // ilgili threade bir driver set ettim
 
             }
         }

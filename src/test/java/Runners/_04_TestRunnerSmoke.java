1c39589e8ebe188e1e3a6347a540d3a12be530e2
emr4706 <emr4706@gmail.com>
2023-11-10 09:13:42 +0100

    add jenkins settings

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",// aşağıdaki bütün testlerden @SmokeTest tag i olanları çalıştır
         features = {"src/test/java/FeatureFiles"},
         glue={"StepDefinitions"}
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
